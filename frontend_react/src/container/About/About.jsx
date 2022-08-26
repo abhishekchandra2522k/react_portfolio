d1373b12c10ab3c78198d8e3c6ea2514e5ee2a3f
abhishekchandra2522k <1ac23456789@gmail.com>
2022-08-26 23:03:17 +0530

    Update About.jsx

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+
 import './About.scss'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <>
+      <h2 className='head-text'>
+        I know That
+        <span>Good Design</span>
+        <br />
+        means
+        <span>Good Business</span>
+      </h2>
+    </>
+  )
 }
 
 export default About
