2b3f0f335c913c0119f9ea676adae67ae5efbb55
abhishekchandra2522k <1ac23456789@gmail.com>
2022-08-28 21:29:57 +0530

    Update About

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,39 +2,48 @@ import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 import { images } from '../../constants'
 import './About.scss'
+import { urlFor, client } from '../../client'
 
-const abouts = [
-  {
-    title: 'Web  Development',
-    description: 'I am good web developer',
-    imgUrl: images.about01,
-  },
-  {
-    title: 'Web  Design',
-    description: 'I am good web developer',
-    imgUrl: images.about02,
-  },
-  {
-    title: 'UI/UX',
-    description: 'I am good web developer',
-    imgUrl: images.about03,
-  },
-  {
-    title: 'Web Animations',
-    description: 'I am good web developer',
-    imgUrl: images.about04,
-  },
-]
+// const abouts = [
+//   {
+//     title: 'Web  Development',
+//     description: 'I am good web developer',
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: 'Frontend  Development',
+//     description: 'I am good web developer',
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: 'Backend  Development',
+//     description: 'I am good web developer',
+//     imgUrl: images.about03,
+//   },
+//   {
+//     title: 'MERN Stack',
+//     description: 'I am good web developer',
+//     imgUrl: images.about04,
+//   },
+// ]
 
 const About = () => {
+  const [abouts, setAbouts] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query).then((data) => {
+      setAbouts(data)
+    })
+  }, [])
+
   return (
     <>
       <h2 className='head-text'>
-        I know That
-        <span>Good Design</span>
+        I know That <span>Good Dev</span>
         <br />
-        means
-        <span>Good Business</span>
+        means <span>Good Business</span>
       </h2>
 
       <div className='app__profiles'>
