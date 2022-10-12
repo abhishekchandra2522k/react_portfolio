e30b277127c0dd374b06aef7c45dc375fbc4327a
abhishekchandra2522k <1ac23456789@gmail.com>
2022-10-12 23:42:17 +0530

    Update App.js, add blog component

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,19 +1,29 @@
 import React from 'react'
-
-import { About, Footer, Header, Skills, Testimonial, Work } from './container'
+import { Route, Routes } from 'react-router-dom'
+import { About, Footer, Header, Skills, Work, Blog } from './container'
 import { Navbar } from './components'
 import './App.scss'
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar />
-      <Header />
-      <About />
-      <Work />
-      <Skills />
-      {/* <Testimonial /> */}
-      <Footer />
+      <Routes>
+        <Route
+          exact
+          path='/'
+          element={
+            <>
+              <Navbar />
+              <Header />
+              <About />
+              <Work />
+              <Skills />
+              <Footer />
+            </>
+          }
+        />
+        <Route exact path='/blog' element={<Blog />} />
+      </Routes>
     </div>
   )
 }
